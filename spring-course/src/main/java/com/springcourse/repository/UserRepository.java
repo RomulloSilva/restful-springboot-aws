07dba06e9f4e5a8e3c50819b6e4e23799bfac0fb
RomulloSilva <romullo.dev@gmail.com>
2021-01-05 19:47:00 -0300

    Adição dos repositórios com métodos da extensão JpaRepository

---
+++ spring-course/src/main/java/com/springcourse/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package com.springcourse.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	/*
+	 * Métodos de fazer um login/ buscar um usuário específico
+	public User getByEmailAndPassword(String email, String password);
+	pro utilizar a anotação repository e a extensão Jpa não é necessário,
+	fazer alguns métodos de busca porque ele ja tem. Ex: findAll() 
+	*/
+	//Login o optional é um método de tratar valores nulos.
+	@Query("SELECT FROM User WHERE email = ?1 AND password = ?2")
+	public Optional<User> login(String email, String password);
+	
+	
+	
+}
